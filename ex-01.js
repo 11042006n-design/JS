255f50f4a27b77dc4fd1367ce54dd1bb94d3c0c9
Nastya <11042006n@gmail.com>
2026-08-11 21:21:31 +0300

    Текстовый опросник

---
+++ ex-01.js
@@ -0,0 +1,23 @@
+let score=0;
+let question="Что означает слово `cat`? [кошка, собака]";
+let answer=prompt(question);
+
+if (answer=="кошка") {
+    score=score+10;
+    alert("все верно!");
+}
+else {
+    alert("нужно еще подумать");
+}
+
+question="Что означает слово `dog`? [кошка, собака]";
+answer=prompt(question);
+if (answer=="собака") {
+    score=score+10;
+    alert("все верно!");
+}
+else {
+    alert("нужно еще подумать");
+}
+
+alert("счет: "+score);
